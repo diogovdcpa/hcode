5c2681019509857ba017b9b26d1abea587416503
Diogo Caldas <diogovdcpa@gmail.com>
2020-06-09 13:10:42 -0300

    manipulando o dom

--- calculator/script/controller/CalcController.js
+++ calculator/script/controller/CalcController.js
@@ -7,8 +7,20 @@ class CalcController {
 
     initalize(){
 
-        
+        let displayCalcEl = document.querySelector("#display");
+        let dateEL = document.querySelector("#data");
+        let timeEL = document.querySelector("#hora");
 
+        let valor = "123456";
+        if(valor.length > 10){
+            displayCalcEl.textContent = "error";
+        }else{
+            displayCalcEl.textContent= valor;
+        }
+
+        dateEL.textContent = "09/06/2020";
+        timeEL.textContent = "13:00";
+        
     }
 
     get displayCalc() {
