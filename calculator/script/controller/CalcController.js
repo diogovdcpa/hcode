87c9595585de94ca167579133856a000e5d765ed
Diogo Caldas <diogovdcpa@gmail.com>
2020-06-10 08:26:16 -0300

    event click

--- calculator/script/controller/CalcController.js
+++ calculator/script/controller/CalcController.js
@@ -8,6 +8,7 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initalize();
+        this.initButtonsEvents();
 
     }
 
@@ -21,9 +22,19 @@ class CalcController {
 
         }, 1000)
 
+    }
 
+    initButtonsEvents(){
 
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
+       buttons.forEach((btn,index) => {
+
+           btn.addEventListener('click',e=>{
+
+               console.log(btn.className.baseVal.replace("btn-",""));
+           });
+       })
     }
 
     setDisplayDateTime() {
